44da716f4c5fc2e8fd69f8a41ff6ae1d3b4c968a
Himanshu <himanshu.mishra@vinsol.com>
2017-09-01 15:24:16 +0530

    classaccessor.rb modified

--- classaccessor.rb
+++ classaccessor.rb
@@ -6,15 +6,12 @@ class Module
   def cattr_reader(*syms, instance_reader: true, instance_accessor: nil)
     syms.each do |sym|
       raise NameError.new("invalid attribute name: #{sym}") unless /\A[_A-Za-z]\w*\z/.match?(sym)
-      class_eval %{
-        @@#{sym} = nil unless defined? @@#{sym}
-        def self.#{sym}
-          @@#{sym}
-        end
-      }
-
-      if instance_reader && instance_accessor
+      if instance_reader || instance_accessor
         class_eval %{
+          @@#{sym} = nil unless defined? @@#{sym}
+          def self.#{sym}
+            @@#{sym}
+          end
           def #{sym}
             @@#{sym}
           end
@@ -26,15 +23,13 @@ class Module
   def cattr_writer(*syms, instance_writer: true, instance_accessor: nil)
     syms.each do |sym|
       raise NameError.new("invalid attribute name: #{sym}") unless /\A[_A-Za-z]\w*\z/.match?(sym)
-      class_eval %{
-        @@#{sym} = nil unless defined? @@#{sym}
-        def self.#{sym}=(obj)
-          @@#{sym} = obj
-        end
-      }
 
-      if instance_writer && instance_accessor
+      if instance_writer || instance_accessor
         class_eval %{
+          @@#{sym} = nil unless defined? @@#{sym}
+          def self.#{sym}=(obj)
+            @@#{sym} = obj
+          end
           def #{sym}=(obj)
             @@#{sym} = obj
           end
@@ -47,7 +42,7 @@ class Module
     if instance_accessor && instance_reader || instance_accessor && instance_writer
       raise RepeatAccessorError.new
     end
-    instance_accessor = instance_reader && instance_reader
+    instance_accessor = instance_reader && instance_writer
     cattr_reader(*syms, instance_reader: instance_reader, instance_accessor: instance_accessor, &blk)
     cattr_writer(*syms, instance_writer: instance_writer, instance_accessor: instance_accessor)
   end
@@ -56,7 +51,7 @@ end
 
 class Person
   begin
-    cattr_accessor :hair_colors, instance_writer: true, instance_reader: true, instance_accessor: false
+    cattr_accessor :hair_colors, instance_reader: true , instance_writer: true
 end
 
 class Male < Person
