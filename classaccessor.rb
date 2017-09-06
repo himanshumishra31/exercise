1c1d312063a11edd5e4ec738f6c09e89fa2b258d
Himanshu <himanshu.mishra@vinsol.com>
2017-09-06 14:41:07 +0530

    classaccessor.rb modified

--- classaccessor.rb
+++ classaccessor.rb
@@ -1,57 +1,56 @@
 class RepeatAccessorError < StandardError
 end
+class NameError < StandardError
+end
 
 class Module
 
-  def cattr_reader(*syms, instance_reader: true, instance_accessor: nil)
+  VALID_ATTRIBUTE_REGEX = /\A[_A-Za-z]\w*\z/
+
+  def cattr_reader(*syms)
     syms.each do |sym|
-      raise NameError.new("invalid attribute name: #{sym}") unless /\A[_A-Za-z]\w*\z/.match?(sym)
-      if instance_reader || instance_accessor
-        class_eval %{
-          @@#{sym} = nil unless defined? @@#{sym}
-          def self.#{sym}
-            @@#{sym}
-          end
-          def #{sym}
-            @@#{sym}
-          end
-        }
+      @@method_name = "#{sym}"
+      class_eval do
+        class << self
+          define_method(@@method_name) { @@sym }
+        end
+
+        define_method(@@method_name) { @@sym }
       end
     end
   end
 
-  def cattr_writer(*syms, instance_writer: true, instance_accessor: nil)
+  def cattr_writer(*syms)
     syms.each do |sym|
-      raise NameError.new("invalid attribute name: #{sym}") unless /\A[_A-Za-z]\w*\z/.match?(sym)
+      class_eval do
+        @@method_name = "#{sym}="
+        class << self
+          define_method(@@method_name) { |val| @@sym = val }
+        end
 
-      if instance_writer || instance_accessor
-        class_eval %{
-          @@#{sym} = nil unless defined? @@#{sym}
-          def self.#{sym}=(obj)
-            @@#{sym} = obj
-          end
-          def #{sym}=(obj)
-            @@#{sym} = obj
-          end
-        }
+        define_method(@@method_name) { |val| @@sym = val }
       end
     end
   end
 
-  def cattr_accessor(*syms, instance_reader: true, instance_writer: true, instance_accessor: nil, &blk)
-    if instance_accessor && instance_reader || instance_accessor && instance_writer
-      raise RepeatAccessorError.new
-    end
-    instance_accessor = instance_reader && instance_writer
-    cattr_reader(*syms, instance_reader: instance_reader, instance_accessor: instance_accessor, &blk)
-    cattr_writer(*syms, instance_writer: instance_writer, instance_accessor: instance_accessor)
+  def cattr_accessor(*syms, instance_reader: nil, instance_writer: nil, instance_accessor: nil)
+    set_default_values(syms, instance_reader, instance_writer, instance_accessor)
+    cattr_reader(*syms) if @instance_reader || @instance_accessor
+    cattr_writer(*syms) if @instance_writer || @instance_accessor
   end
 
+  def set_default_values(syms, instance_reader, instance_writer, instance_accessor)
+    raise RepeatAccessorError, 'Access repeated' unless instance_accessor.nil? || (instance_reader.nil? && instance_writer.nil?)
+    @instance_reader = true if instance_reader.nil?
+    @instance_writer = true if instance_writer.nil?
+    @instance_accessor = instance_reader && instance_writer if instance_accessor.nil?
+    raise NameError, 'invalid attribute' unless syms.all? { |sym| VALID_ATTRIBUTE_REGEX.match?(sym) }
+  end
 end
 
 class Person
   begin
-    cattr_accessor :hair_colors, instance_reader: true , instance_writer: true
+  cattr_accessor :hair_colors, instance_writer: true, instance_reader: true
 end
 
 class Male < Person
@@ -59,7 +58,6 @@ end
 
 Person.hair_colors = [1,2,3]
 p Person.hair_colors
-
 p Person.new.hair_colors
 p Male.hair_colors
 Male.hair_colors = ['a','b']
