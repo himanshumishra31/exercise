0f743a1a48736f55dc7c7ddfdd1e680bd19a68ae
Himanshu <himanshu.mishra@vinsol.com>
2017-09-07 12:52:09 +0530

    changes done

--- classaccessor.rb
+++ classaccessor.rb
@@ -7,17 +7,24 @@ class Module
 
   VALID_ATTRIBUTE_REGEX = /\A[_A-Za-z]\w*\z/
 
+  attr_accessor :instance_reader, :instance_writer, :instance_accessor
+
   def cattr_reader(*syms)
     syms.each do |sym|
       @@method_name = "#{sym}"
       class_eval do
         class << self
-          define_method(@@method_name) { @@sym }
+          read_method(@@method_name)
         end
-
-        define_method(@@method_name) { @@sym }
+        if instance_reader
+          read_method(@@method_name)
+        end
       end
     end
+  end
+
+  def read_method(method_name)
+    define_method(method_name) { @@sym }
   end
 
   def cattr_writer(*syms)
@@ -25,28 +32,49 @@ class Module
       class_eval do
         @@method_name = "#{sym}="
         class << self
-          define_method(@@method_name) { |val| @@sym = val }
+          write_method(@@method_name)
         end
-
-        define_method(@@method_name) { |val| @@sym = val }
+        if instance_writer
+          write_method(@@method_name)
+        end
       end
     end
   end
 
-  def cattr_accessor(*syms, instance_reader: nil, instance_writer: nil, instance_accessor: nil)
-    set_default_values(syms, instance_reader, instance_writer, instance_accessor)
-    cattr_reader(*syms) if @instance_reader || @instance_accessor
-    cattr_writer(*syms) if @instance_writer || @instance_accessor
+  def write_method(method_name)
+    define_method(method_name) { |val| @@sym = val }
   end
 
-  def set_default_values(syms, instance_reader, instance_writer, instance_accessor)
-    raise RepeatAccessorError, 'Access repeated' unless instance_accessor.nil? || (instance_reader.nil? && instance_writer.nil?)
-    @instance_reader = true if instance_reader.nil?
-    @instance_writer = true if instance_writer.nil?
-    @instance_accessor = instance_reader && instance_writer if instance_accessor.nil?
+  def set_accessors(syms)
+    repeat_accessor_check
+    set_default_values
+    attribute_validity(syms)
+  end
+
+  def repeat_accessor_check
+    raise RepeatAccessorError, 'Accessor repeated' unless instance_accessor.nil? || (instance_reader.nil? && instance_writer.nil?)
+  end
+
+  def set_default_values
+    instance_reader = true if instance_reader.nil?
+    instance_writer = true if instance_writer.nil?
+    instance_accessor = instance_reader && instance_writer if instance_accessor.nil?
+  end
+
+  def attribute_validity(syms)
     raise NameError, 'invalid attribute' unless syms.all? { |sym| VALID_ATTRIBUTE_REGEX.match?(sym) }
   end
+
+  def cattr_accessor(*syms, instance_reader: nil, instance_writer: nil, instance_accessor: nil)
+    self.instance_reader = instance_reader
+    self.instance_writer = instance_writer
+    self.instance_accessor = instance_accessor
+    set_accessors(syms)
+    cattr_reader(*syms, instance_reader)
+    cattr_writer(*syms, instance_writer)
+  end
 end
+
 
 class Person
   begin
