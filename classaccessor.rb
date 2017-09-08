6614707a6e80c2cb2c22d0355174608a356e8cc3
Himanshu <himanshu.mishra@vinsol.com>
2017-09-08 18:22:26 +0530

    modified

--- classaccessor.rb
+++ classaccessor.rb
@@ -1,7 +1,5 @@
-class RepeatAccessorError < StandardError
-end
-class NameError < StandardError
-end
+class RepeatAccessorError < StandardError ; end
+class NameError < StandardError ; end
 
 class Module
 
@@ -9,40 +7,33 @@ class Module
 
   attr_accessor :instance_reader, :instance_writer, :instance_accessor
 
-  def cattr_reader(*syms)
-    syms.each do |sym|
-      @@method_name = "#{sym}"
-      class_eval do
-        class << self
-          read_method(@@method_name)
-        end
-        if instance_reader
-          read_method(@@method_name)
+  def create_accessor_method
+    arr = ['reader','writer']
+    arr.each do |name|
+      self.class.class_eval do
+        define_method("cattr_#{name}") do |*syms,instance|
+          syms.each do |sym|
+            @@sym = sym
+            @@method_name = name
+            class << self
+              instance_eval "#{@@method_name}_method(@@sym)"
+            end
+            if instance
+              instance_eval "#{@@method_name}_method(@@sym)"
+            end
+          end
         end
       end
     end
   end
 
-  def read_method(method_name)
+  def reader_method(method_name)
     define_method(method_name) { @@sym }
   end
 
-  def cattr_writer(*syms)
-    syms.each do |sym|
-      class_eval do
-        @@method_name = "#{sym}="
-        class << self
-          write_method(@@method_name)
-        end
-        if instance_writer
-          write_method(@@method_name)
-        end
-      end
-    end
-  end
 
-  def write_method(method_name)
-    define_method(method_name) { |val| @@sym = val }
+  def writer_method(method_name)
+    define_method("#{method_name}=") { |val| @@sym = val }
   end
 
   def set_accessors(syms)
@@ -70,29 +61,27 @@ class Module
     self.instance_writer = instance_writer
     self.instance_accessor = instance_accessor
     set_accessors(syms)
-    cattr_reader(*syms, instance_reader)
-    cattr_writer(*syms, instance_writer)
+    create_accessor_method
+    cattr_reader(*syms,instance_reader)
+    cattr_writer(*syms,instance_writer)
   end
 end
 
-
 class Person
-  begin
   cattr_accessor :hair_colors, instance_writer: true, instance_reader: true
 end
 
 class Male < Person
 end
 
-Person.hair_colors = [1,2,3]
-p Person.hair_colors
-p Person.new.hair_colors
-p Male.hair_colors
-Male.hair_colors = ['a','b']
-p Male.hair_colors
-Male.hair_colors << 'c'
-p Person.hair_colors
-p Male.new.hair_colors
-rescue StandardError => e
-  puts e.message
+begin
+  Person.hair_colors = [1,2,3]
+  p Person.hair_colors
+  p Person.new.hair_colors
+  p Male.hair_colors
+  Male.hair_colors = ['a','b']
+  p Male.hair_colors
+  Male.hair_colors << 'c'
+  p Person.hair_colors
+  p Male.new.hair_colors
 end
